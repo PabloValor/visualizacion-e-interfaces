5e7854b15c8ec0a40ef0ba4181c93e09d60bd4f3
Pablo Valor <pablovalor89@gmail.com>
2016-07-06 09:50:27 -0300

    Se hacen cambios varios

--- assets/js/components/layout.js
+++ assets/js/components/layout.js
@@ -12,39 +12,40 @@ var Layout = function() {
 
 	function cargarSettings($window, $header) {
 		var $buttonCollapse = $('.button-collapse');
-		
+
 		// header
 		$header.height($window.height());
 
 		// particlesground
-		$header.particleground({
+		var particlesOptions = {
 			dotColor: 'ghostwhite',
-			lineColor: 'gray',
+			lineColor: isPage('widget') ? 'white': 'gray',
 			directionX: 'center',
 			directionY: 'center',
 			density: 10000,
 			particleRadius: 4,
 			lineWidth: .5
-		});
+		}
+		$header.particleground(particlesOptions);
 
 		// menu mobile
 		$buttonCollapse.sideNav();
 
 	}
 	function cargarEventos($window, $header, $nav, CounterFlag) {
-		if(isContactPage()) {
+		if(isPage('contacto')) {
 			bgColorNav($nav, true);
 		}
     	// scroll nav color
     	$window.scroll(function() {
     		changeNavColor($window, $header, $nav);
-			
+
     		// TODO: Refactor
 			var scrollPosition = scrollY || pageYOffset;
 			if (scrollPosition > $("#counter").position().top - $window.height() && CounterFlag) {
 				CounterFlag = false;
 				$('.timer').countTo();
-			}    		
+			}
     	});
 	}
 
@@ -53,16 +54,17 @@ var Layout = function() {
 			bgColorNav($nav, true);
 			} else {
 			bgColorNav($nav, false);
-		}		
+		}
 	}
 
 	function bgColorNav($nav, show) {
-		$nav.css({ 
+		$nav.css({
 			background: show ? '#2B373A' : 'transparent'
 		});
 	}
 
-	function isContactPage() {
-		return window.location.pathname.includes('contacto');
+	function isPage(name) {
+		return window.location.pathname.includes(name);
 	}
+
 };
